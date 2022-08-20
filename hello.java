74c2b48ca2a9339800774d74c0d38e1dab12b295
Omkar Auti <57768352+theomkarauti@users.noreply.github.com>
2022-08-20 16:45:44 +0530

    multiple lines added.

--- hello.java
+++ hello.java
@@ -5,8 +5,13 @@ class hello
 	{
 	
 	
-		System.out.println("hello called");
-	
+		System.out.println("hello called 1");
+		System.out.println("hello called 2");
+		System.out.println("hello called 3");
+		System.out.println("hello called 4");
+		System.out.println("hello called 5");
+		System.out.println("hello called 6");
+	System.out.println("hello called 6");
 	
 	}
 
